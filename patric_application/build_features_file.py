f0e6ee1bedf630ff92c9d1476b5a973c3b48f15d
Pouki1999 <gogo.maslyankov@gmail.com>
2021-06-29 13:08:40 -0400

    Test with new file (building features matrix from specialty genes)

---
+++ patric_application/build_features_file.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import os
+
+folder = 'data_files/sp_genes/ciprofloxacin'
+
+for dir in os.listdir(folder):
+    print(dir)
+    for genome in os.listdir(dif):
+        df = pd.read_csv(os.path.join(dir, genome))
+        print(df)
+        print(df['function classification'])
+    break
