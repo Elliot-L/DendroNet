937d6003997357dcaa93187bd17ae788848c7d93
Pouki1999 <gogo.maslyankov@gmail.com>
2021-06-29 13:19:00 -0400

    Small fixes again

--- patric_application/build_features_file.py
+++ patric_application/build_features_file.py
@@ -4,10 +4,10 @@ import os
 folder = 'data_files/sp_genes/ciprofloxacin'
 
 for directory in os.listdir(folder):
-    print(dir)
+    print(directory)
     for genome in os.listdir(os.path.join(folder, directory)):
         print(genome)
-        df = pd.read_csv(os.path.join(folder, directory, genome))
+        df = pd.read_csv(os.path.join(folder, directory, genome), sep='\t')
         print(df)
         print(df['function classification'])
     break
