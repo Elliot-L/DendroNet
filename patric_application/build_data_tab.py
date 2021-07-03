1a2bd5cb797c755f929ff649b5be9f631aef71d3
Pouki1999 <gogo.maslyankov@gmail.com>
2021-07-02 21:19:06 -0400

    Fix bug in build data tab file

--- patric_application/build_data_tab.py
+++ patric_application/build_data_tab.py
@@ -3,42 +3,46 @@ import os
 import jsonpickle
 import json
 
-def build_tab(seeds=[0,1,2,3,4]):
-    data = {}
-    data['antibiotic'] = []
-    data['group'] = []
-    data['LR'] = []
-    data['DPF'] = []
-    data['L1'] = []
-    data['Early Stopping'] = []
-    data['Seed'] = []
-    data['Val AUC'] = []
-    data['Test AUC'] = []
-    data['Sensitivity'] = []
-    data['Specificity'] = []
-    for dir in os.listdir(os.path.join('data_files', 'patric_tuning')):
-        with open(os.path.join('data_files', 'patric_tuning', dir, 'output.json')) as file:
-            JSdict = json.load(file)
-            for i, seed in enumerate(seeds):
-                data['antibiotic'].append(dir.split("_")[0])
-                data['group'].append(dir.split("_")[1])
-                data['LR'].append(dir.split("_")[4])
-                data['DPF'].append(dir.split("_")[3])
-                data['L1'].append(dir.split("_")[5])
-                data['Early Stopping'].append(dir.split("_")[6])
-                data['Seed'].append(seed)
-                data['Val AUC'].append(JSdict['val_auc'][i])
-                data['Test AUC'].append(JSdict['test_auc'][i])
-                data['Sensitivity'].append(JSdict['test_sensitivity'][i])
-                data['Specificity'].append(JSdict['test_specificity'][i])
-    df = pd.DataFrame(data=data)
+def build_tab(df_file=None ,seeds=[0, 1, 2]):
+    if df_file == None:
+        data = {}
+        data['antibiotic'] = []
+        data['group'] = []
+        data['LR'] = []
+        data['DPF'] = []
+        data['L1'] = []
+        data['Early Stopping'] = []
+        data['Seed'] = []
+        data['Val AUC'] = []
+        data['Test AUC'] = []
+        data['Sensitivity'] = []
+        data['Specificity'] = []
+        for dir in os.listdir(os.path.join('data_files', 'patric_tuning')):
+            with open(os.path.join('data_files', 'patric_tuning', dir, 'output.json')) as file:
+                JSdict = json.load(file)
+                for i, seed in enumerate(seeds):
+                    data['antibiotic'].append(dir.split("_")[0])
+                    data['group'].append(dir.split("_")[1])
+                    data['LR'].append(dir.split("_")[4])
+                    data['DPF'].append(dir.split("_")[3])
+                    data['L1'].append(dir.split("_")[5])
+                    data['Early Stopping'].append(dir.split("_")[6])
+                    data['Seed'].append(seed)
+                    data['Val AUC'].append(JSdict['val_auc'][i])
+                    data['Test AUC'].append(JSdict['test_auc'][i])
+                    data['Sensitivity'].append(JSdict['test_sensitivity'][i])
+                    data['Specificity'].append(JSdict['test_specificity'][i])
+        df = pd.DataFrame(data=data)
+    else:
+        df = pd.read_csv(os.path.join('data_files', 'Results', df_file))
+
     print(df)
 
     best_combs = {}
     val_averages = {}
     test_averages = {}
 
-    for row in range(0, df.shape[0], 5):
+    for row in range(0, df.shape[0], len(seeds)):
         val_average_auc = 0.0
         test_average_auc = 0.0
         for seed in range(len(seeds)):
@@ -47,20 +51,30 @@ def build_tab(seeds=[0,1,2,3,4]):
         val_average_auc = val_average_auc / len(seeds)
         test_average_auc = test_average_auc / len(seeds)
 
-        name = df['antibiotic'][row] + '_' +df['group'][row]
+        name = df['antibiotic'][row] + '_' + df['group'][row]
 
         if name in best_combs:
             if val_average_auc > val_averages[name]:
-                best_combs[name] = (df['LR'][row], df['DPF'][row], df['L1'][row])
+                best_combs[name] = ["LR:", df['LR'][row], "DPF:", df['DPF'][row], "L1", df['L1'][row], 'Early Stop:', df['Early Stopping'][row]]
                 val_averages[name] = val_average_auc
+                test_averages[name] = test_average_auc
         else:
-            best_combs[name] = ("LR:", df['LR'][row], "DPF:", df['DPF'][row], "L1", df['L1'][row])
+            best_combs[name] = ["LR:", df['LR'][row], "DPF:", df['DPF'][row], "L1", df['L1'][row], 'Early Stop:', df['Early Stopping'][row]]
             val_averages[name] = val_average_auc
             test_averages[name] = test_average_auc
 
     print(best_combs)
     print(val_averages)
     print(test_averages)
+
+    os.makedirs(os.path.join('data_files', 'Results'), exist_ok=True)
+    df.to_csv(os.path.join('data_files', 'Results', 'brute_results_' + str(set(data['antibiotic'])) + '.csv'), index=False)
+
+    output_list = [best_combs, val_averages, test_averages]
+
+    with open(os.path.join('data_files', 'Results', 'refined_results' + str(set(data['antibiotic'])) + '.json'), 'w') as outfile:
+        json.dump(output_list, outfile)
+
     return df, best_combs, val_averages, test_averages
 
 
