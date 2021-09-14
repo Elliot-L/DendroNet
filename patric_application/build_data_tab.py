d4beb69eae735a910b2da60d79b43cc47108866d
Pouki1999 <gogo.maslyankov@gmail.com>
2021-09-13 23:28:46 -0400

    making build data tab easier to use

--- patric_application/build_data_tab.py
+++ patric_application/build_data_tab.py
@@ -30,6 +30,26 @@ def build_tab(antibiotic, group, model, leaf_level, seeds=[0, 1, 2, 3, 4]):
                         data['Val AUC'].append(JSdict['val_auc'][i])
                         data['Test AUC'].append(JSdict['test_auc'][i])
 
+        df = pd.DataFrame(data=data)
+        # print(df)
+
+        results = {'validation_average': -1}
+
+        for row in range(0, df.shape[0], len(seeds)):
+            val_average_auc = 0.0
+            test_average_auc = 0.0
+            for seed in range(len(seeds)):
+                val_average_auc += df['Val AUC'][row + seed]
+                test_average_auc += df['Test AUC'][row + seed]
+            val_average_auc = val_average_auc / len(seeds)
+            test_average_auc = test_average_auc / len(seeds)
+
+            if val_average_auc > results['validation_average']:
+                results['best_combinations'] = ["LR:", df['LR'][row], "DPF:", df['DPF'][row], "L1", df['L1'][row],
+                                                'Early Stop:', df['Early Stopping'][row]]
+                results['validation_average'] = val_average_auc
+                results['test_average'] = test_average_auc
+
     elif model == 'logistic':
         data = {}
         data['LR'] = []
@@ -50,29 +70,28 @@ def build_tab(antibiotic, group, model, leaf_level, seeds=[0, 1, 2, 3, 4]):
                         data['Val AUC'].append(JSdict['val_auc'][i])
                         data['Test AUC'].append(JSdict['test_auc'][i])
 
+        df = pd.DataFrame(data=data)
+        # print(df)
 
-    df = pd.DataFrame(data=data)
-    #print(df)
+        results = {'validation_average': -1}
 
-    results = { 'validation_average': -1}
+        for row in range(0, df.shape[0], len(seeds)):
+            val_average_auc = 0.0
+            test_average_auc = 0.0
+            for seed in range(len(seeds)):
+                val_average_auc += df['Val AUC'][row + seed]
+                test_average_auc += df['Test AUC'][row + seed]
+            val_average_auc = val_average_auc / len(seeds)
+            test_average_auc = test_average_auc / len(seeds)
 
-    for row in range(0, df.shape[0], len(seeds)):
-        val_average_auc = 0.0
-        test_average_auc = 0.0
-        for seed in range(len(seeds)):
-            val_average_auc += df['Val AUC'][row + seed]
-            test_average_auc += df['Test AUC'][row + seed]
-        val_average_auc = val_average_auc / len(seeds)
-        test_average_auc = test_average_auc / len(seeds)
-
-
-        if val_average_auc > results['validation_average']:
-            results['best_combinations'] = ["LR:", df['LR'][row], "DPF:", df['DPF'][row], "L1", df['L1'][row], 'Early Stop:', df['Early Stopping'][row]]
-            results['validation_average'] = val_average_auc
-            results['test_average'] = test_average_auc
+            if val_average_auc > results['validation_average']:
+                results['best_combinations'] = ["LR:", df['LR'][row], 'Early Stop:', df['Early Stopping'][row]]
+                results['validation_average'] = val_average_auc
+                results['test_average'] = test_average_auc
 
     print(results)
     print(df)
+
     if leaf_level == 'none':
         df_file = os.path.join('data_files', 'Results', 'brute_results_' + group
                                + '_' + antibiotic + '_' + model + '.csv')
