a581338b0154c4c3b06ed6ced99bfe9232d1dfb1
Pouki1999 <gogo.maslyankov@gmail.com>
2021-09-22 23:46:58 -0400

    Fix problem with logistic file

--- patric_application/log_experiment.py
+++ patric_application/log_experiment.py
@@ -70,7 +70,8 @@ if __name__ == '__main__':
         # Used for comparison with DendroNet performance
         # We use a linear regression in order to be able to use BCEWithLogitsLoss as loss function,
         # a more stable version of BCEloss
-        logistic = LinRegModel(len(X[0]), device=device)
+        logistic = LinRegModel(len(X[0]))
+        logistic.to(device)
 
         train_idx, test_idx = split_indices(range(len(X)), seed=0)
         train_idx, val_idx = split_indices(train_idx, seed=s)
