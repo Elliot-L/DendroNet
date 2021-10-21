4ae46c7dd12945f6b92295c116bd753e3795dcad
Pouki1999 <gogo.maslyankov@gmail.com>
2021-10-21 17:59:56 -0400

    fix mistake

--- patric_application/clean_invalid_tuning.py
+++ patric_application/clean_invalid_tuning.py
@@ -4,7 +4,7 @@ import json
 if __name__ == '__main__':
     for directory in os.listdir(os.path.join('data_files', 'patric_tuning')):
         file = os.path.join('data_files', 'patric_tuning', directory, 'output.json')
-        with open(file, 'w') as f:
+        with open(file) as f:
             dic = json.load(f)
         if len(dic) > 3:
             d = os.path.join('data_files', 'patric_tuning', directory)
