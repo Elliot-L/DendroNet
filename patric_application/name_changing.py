eb97696501ce4cced73c2c88cad3145f6a053f88
Pouki1999 <gogo.maslyankov@gmail.com>
2021-12-02 21:27:41 -0500

    cleaning names file

--- patric_application/name_changing.py
+++ patric_application/name_changing.py
@@ -26,7 +26,7 @@ if __name__ ==  '__main__':
                 new_name = os.path.join(args.directory, new_name)
                 print(old_name)
                 print(new_name)
-                #os.system('mv ' + old_name + ' ' + new_name)
+                os.system('mv ' + old_name + ' ' + new_name)
     elif args.change == 'Bacteria':
         for d in os.listdir(args.directory):
             name_list = d.split('_')
