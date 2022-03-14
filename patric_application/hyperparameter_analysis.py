86125d1255b184e4a1e8423ea9edfce5e1693d68
Pouki1999 <gogo.maslyankov@gmail.com>
2022-03-14 17:56:32 -0400

    Adding more hyperparameter values to test

--- patric_application/hyperparameter_analysis.py
+++ patric_application/hyperparameter_analysis.py
@@ -18,7 +18,23 @@ if __name__ == '__main__':
     dendronet_df = pd.read_csv(dendronet_results_file, dtype=str)
     logistic_df = pd.read_csv(logistic_results_file, dtype=str)
 
+    dpfs = []
+    lrs = []
+    l1s = []
+    early_stops = []
+
     for row in range(dendronet_df.shape[0]):
+        if dendronet_df.loc[row, 'DPF'] not in dpfs:
+            dpfs.append(dendronet_df.loc[row, 'DPF'])
+        if dendronet_df.loc[row, 'LR'] not in lrs:
+            lrs.append(dendronet_df.loc[row, 'LR'])
+        if dendronet_df.loc[row, 'L1'] not in l1s:
+            l1s.append(dendronet_df.loc[row, 'L1'])
+        if dendronet_df.loc[row, 'Early Stopping'] not in early_stops:
+            early_stops.append(dendronet_df.loc[row, 'Early Stopping'])
+
+
+
 
 
 
