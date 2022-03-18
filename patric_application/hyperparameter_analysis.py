eeec5f618729883067beac863f3245e7ebbf30f9
Pouki1999 <gogo.maslyankov@gmail.com>
2022-03-18 14:14:52 -0400

    add train AUC to analysis

--- patric_application/hyperparameter_analysis.py
+++ patric_application/hyperparameter_analysis.py
@@ -43,6 +43,8 @@ if __name__ == '__main__':
     data['LR'] = []
     data['L1'] = []
     data['Early Stop'] = []
+    data['Train mean'] = []
+    data['Train variance'] = []
     data['Val mean'] = []
     data['Val variance'] = []
     data['Test mean'] = []
@@ -50,28 +52,37 @@ if __name__ == '__main__':
 
     #print("DPFs: ")
     for dpf in dpfs:
+        train_list = []
         test_list = []
         val_list = []
         for row in range(dendronet_df.shape[0]):
             if dendronet_df.loc[row, 'DPF'] == dpf:
+                train_list.append(float(dendronet_df.loc[row, 'Train AUC']))
                 test_list.append(float(dendronet_df.loc[row, 'Test AUC']))
                 val_list.append(float(dendronet_df.loc[row, 'Val AUC']))
-        #print(test_list)
-        #print(val_list)
+        train_average = 0
         test_average = 0
         val_average = 0
+        for t in train_list:
+            train_average += t
         for t in test_list:
             test_average += t
         for v in val_list:
             val_average += v
+        train_average = train_average / len(train_list)
         test_average = test_average / len(test_list)
         val_average = val_average / len(val_list)
+        train_variance = 0
         test_variance = 0
         val_variance = 0
+        for t in train_list:
+            train_variance += (t - train_average) ** 2
         for t in test_list:
             test_variance += (t - test_average) ** 2
         for v in val_list:
             val_variance += (v - val_average) ** 2
+
+        train_variance = train_variance / (len(train_list) - 1)
         test_variance = test_variance / (len(test_list) - 1)
         val_variance = val_variance / (len(val_list) - 1)
 
@@ -79,6 +90,8 @@ if __name__ == '__main__':
         data['LR'].append('-')
         data['L1'].append('-')
         data['Early Stop'].append('-')
+        data['Train mean'].append(train_average)
+        data['Train variance'].append(train_variance)
         data['Val mean'].append(val_average)
         data['Val variance'].append(val_variance)
         data['Test mean'].append(test_average)
@@ -86,28 +99,37 @@ if __name__ == '__main__':
 
     #print("LRs: ")
     for lr in lrs:
+        train_list = []
         test_list = []
         val_list = []
         for row in range(dendronet_df.shape[0]):
             if dendronet_df.loc[row, 'LR'] == lr:
+                train_list.append(float(dendronet_df.loc[row, 'Train AUC']))
                 test_list.append(float(dendronet_df.loc[row, 'Test AUC']))
                 val_list.append(float(dendronet_df.loc[row, 'Val AUC']))
-        #print(test_list)
-        #print(val_list)
+        train_average = 0
         test_average = 0
         val_average = 0
+        for t in train_list:
+            train_average += t
         for t in test_list:
             test_average += t
         for v in val_list:
             val_average += v
+        train_average = train_average / len(train_list)
         test_average = test_average / len(test_list)
         val_average = val_average / len(val_list)
+        train_variance = 0
         test_variance = 0
         val_variance = 0
+        for t in train_list:
+            train_variance += (t - train_average) ** 2
         for t in test_list:
             test_variance += (t - test_average) ** 2
         for v in val_list:
             val_variance += (v - val_average) ** 2
+
+        train_variance = train_variance / (len(train_list) - 1)
         test_variance = test_variance / (len(test_list) - 1)
         val_variance = val_variance / (len(val_list) - 1)
 
@@ -115,6 +137,8 @@ if __name__ == '__main__':
         data['LR'].append(lr)
         data['L1'].append('-')
         data['Early Stop'].append('-')
+        data['Train mean'].append(train_average)
+        data['Train variance'].append(train_variance)
         data['Val mean'].append(val_average)
         data['Val variance'].append(val_variance)
         data['Test mean'].append(test_average)
@@ -122,28 +146,36 @@ if __name__ == '__main__':
 
     #print("L1s: ")
     for l1 in l1s:
+        train_list = []
         test_list = []
         val_list = []
         for row in range(dendronet_df.shape[0]):
             if dendronet_df.loc[row, 'L1'] == l1:
+                train_list.append(float(dendronet_df.loc[row, 'Train AUC']))
                 test_list.append(float(dendronet_df.loc[row, 'Test AUC']))
                 val_list.append(float(dendronet_df.loc[row, 'Val AUC']))
-        #print(test_list)
-        #print(val_list)
+        train_average = 0
         test_average = 0
         val_average = 0
+        for t in train_list:
+            train_average += t
         for t in test_list:
             test_average += t
         for v in val_list:
             val_average += v
+        train_average = train_average / len(train_list)
         test_average = test_average / len(test_list)
         val_average = val_average / len(val_list)
+        train_variance = 0
         test_variance = 0
         val_variance = 0
+        for t in train_list:
+            train_variance = (t - train_average) ** 2
         for t in test_list:
             test_variance += (t - test_average) ** 2
         for v in val_list:
             val_variance += (v - val_average) ** 2
+        train_variance = train_variance / (len(train_list) - 1)
         test_variance = test_variance / (len(test_list) - 1)
         val_variance = val_variance / (len(val_list) - 1)
 
@@ -151,6 +183,8 @@ if __name__ == '__main__':
         data['LR'].append('-')
         data['L1'].append(l1)
         data['Early Stop'].append('-')
+        data['Train mean'].append(train_average)
+        data['Train variance'].append(train_variance)
         data['Val mean'].append(val_average)
         data['Val variance'].append(val_variance)
         data['Test mean'].append(test_average)
@@ -158,28 +192,36 @@ if __name__ == '__main__':
 
     #print("Early Stops: ")
     for early in early_stops:
+        train_list = []
         test_list = []
         val_list = []
         for row in range(dendronet_df.shape[0]):
             if dendronet_df.loc[row, 'Early Stopping'] == early:
+                train_list.append(float(dendronet_df.loc[row, 'Train AUC']))
                 test_list.append(float(dendronet_df.loc[row, 'Test AUC']))
                 val_list.append(float(dendronet_df.loc[row, 'Val AUC']))
-        #print(test_list)
-        #print(val_list)
+        train_average = 0
         test_average = 0
         val_average = 0
+        for t in train_list:
+            train_average += t
         for t in test_list:
             test_average += t
         for v in val_list:
             val_average += v
+        train_average = train_average / len(train_list)
         test_average = test_average / len(test_list)
         val_average = val_average / len(val_list)
+        train_variance = 0
         test_variance = 0
         val_variance = 0
+        for t in train_list:
+            train_variance += (t - train_average) ** 2
         for t in test_list:
             test_variance += (t - test_average) ** 2
         for v in val_list:
             val_variance += (v - val_average) ** 2
+        train_variance = train_variance / (len(train_list) - 1)
         test_variance = test_variance / (len(test_list) - 1)
         val_variance = val_variance / (len(val_list) - 1)
 
@@ -187,6 +229,8 @@ if __name__ == '__main__':
         data['LR'].append('-')
         data['L1'].append('-')
         data['Early Stop'].append(early)
+        data['Train mean'].append(train_average)
+        data['Train variance'].append(train_variance)
         data['Val mean'].append(val_average)
         data['Val variance'].append(val_variance)
         data['Test mean'].append(test_average)
