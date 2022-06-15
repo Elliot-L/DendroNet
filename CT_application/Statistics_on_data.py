0621854a62e96274d87954ac3a035b7dc9bdd33e
Pouki1999 <gogo.maslyankov@gmail.com>
2022-06-15 15:28:09 -0400

    final version of stats file

--- CT_application/Statistics_on_data.py
+++ CT_application/Statistics_on_data.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     with open(os.path.join('data_files', 'enhancers_seqs.json'), 'r') as jfile:
         enhancer_dict = json.load(jfile)
 
+    enhancers_list = enhancer_dict.keys()
     cell_names = []
 
     features = ['active', 'repressed', 'bivalent', 'proximal', 'distal', 'CTCF', 'nonCTCF', 'AS', 'nonAS']
@@ -20,7 +21,9 @@ if __name__ == '__main__':
         cell_name = ct_file[0:-29]
         cell_names.append(cell_name)
         print(cell_name)
-        cell_df = pd.read_csv(os.path.join('data_files', 'CT_enhancer_features_matrices', ct_file), dtype=str)
+        cell_df = pd.read_csv(os.path.join('data_files', 'CT_enhancer_features_matrices', ct_file), index_col='cCRE_id',
+                              dtype=str)
+        cell_df = cell_df.loc[enhancers_list]
         print(cell_df)
 
         cell_dict = {'active': 0, 'repressed': 0, 'bivalent': 0, 'act_rep_biv': 0,
@@ -31,18 +34,17 @@ if __name__ == '__main__':
         for row in range(cell_df.shape[0]):
             if row % 10000 == 0:
                 print(row)
-            if cell_df.loc[row, 'cCRE_id'] in enhancer_dict:
-                for feature in features:
-                    if cell_df.loc[row, feature] == '1':
-                        cell_dict[feature] += 1
-                if cell_df.loc[row, 'active'] == '1' and cell_df.loc[row, 'repressed'] == '1' and cell_df.loc[row, 'bivalent'] == '1':
-                    cell_dict['act_rep_biv'] += 1
-                if cell_df.loc[row, 'proximal'] == '1' and cell_df.loc[row, 'distal'] == '1':
-                    cell_dict['both_positions'] += 1
-                if cell_df.loc[row, 'CTCF'] == '1' and cell_df.loc[row, 'nonCTCF'] == '1':
-                    cell_dict['both_CTCF'] += 1
-                if cell_df.loc[row, 'AS'] == '1' and cell_df.loc[row, 'nonAS'] == '1':
-                    cell_dict['both_AS'] += 1
+            for feature in features:
+                if cell_df.loc[row, feature] == '1':
+                    cell_dict[feature] += 1
+            if cell_df.loc[row, 'active'] == '1' and cell_df.loc[row, 'repressed'] == '1' and cell_df.loc[row, 'bivalent'] == '1':
+                cell_dict['act_rep_biv'] += 1
+            if cell_df.loc[row, 'proximal'] == '1' and cell_df.loc[row, 'distal'] == '1':
+                cell_dict['both_positions'] += 1
+            if cell_df.loc[row, 'CTCF'] == '1' and cell_df.loc[row, 'nonCTCF'] == '1':
+                cell_dict['both_CTCF'] += 1
+            if cell_df.loc[row, 'AS'] == '1' and cell_df.loc[row, 'nonAS'] == '1':
+                cell_dict['both_AS'] += 1
 
         for feature in data.keys():
             data[feature].append(cell_dict[feature])
