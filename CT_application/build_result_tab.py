cea620522d6f122eaf1eb5e2de4085552abf4a5a
Pouki1999 <gogo.maslyankov@gmail.com>
2022-08-14 18:28:36 -0400

    updating build result tab file to include the new embedding baseline

--- CT_application/build_result_tab.py
+++ CT_application/build_result_tab.py
@@ -13,9 +13,14 @@ if __name__ == '__main__':
                           'average_train_AUC': [], 'average_val_AUC': [], 'average_test_AUC': [],
                           'average_train_loss': [], 'average_val_loss': [], 'average_test_loss': [],
                           'average_epochs': []}
-    dendro_data = {'feature': [], 'LR': [], 'L1': [], 'DPF': [], 'embedding_size': [], 'early_stop': [],
+    embedding_data = {'feature': [], 'LR': [], 'EL': [], 'embedding_size': [], 'early_stop': [],
                    'type_data': [], 'average_train_AUC': [], 'average_val_AUC': [], 'average_test_AUC': [],
                    'average_train_loss': [], 'average_val_loss': [], 'average_test_loss': [], 'average_epochs': []}
+
+    dendro_data = {'feature': [], 'LR': [], 'L1': [], 'DPF': [], 'embedding_size': [], 'early_stop': [],
+                   'type_data': [], 'average_train_AUC': [], 'average_val_AUC': [], 'average_test_AUC': [],
+                   'average_train_loss': [], 'average_val_loss': [], 'average_test_loss': [], 'average_epochs': [],
+                   'tree': []}
 
     print('Single tissue experiments:')
     for tissue_dir in os.listdir(os.path.join('results', 'single_tissue_experiments')):
@@ -39,18 +44,16 @@ if __name__ == '__main__':
             #average_test_loss = 0.0
             average_epochs = 0.0
 
-            for train, val, test in zip(auc_dict['train_auc'], auc_dict['val_auc'], auc_dict['test_auc']):
+            for train, val, test, epoch in zip(auc_dict['train_auc'], auc_dict['val_auc'], auc_dict['test_auc'], auc_dict['epochs']):
                 average_train_auc += train
                 average_val_auc += val
                 average_test_auc += test
+                average_epochs += epoch
 
             #for train, val, test in zip(auc_dict['train_loss'], auc_dict['val_loss'], auc_dict['test_loss']):
              #   average_train_loss += train
             #    average_val_loss += val
             #    average_test_loss += test
-
-            for epoch in auc_dict['epochs']:
-                average_epochs += epoch
 
             single_tissue_data['average_train_AUC'].append(average_train_auc / len(auc_dict['train_auc']))
             single_tissue_data['average_val_AUC'].append(average_val_auc / len(auc_dict['val_auc']))
@@ -78,35 +81,81 @@ if __name__ == '__main__':
         average_train_auc = 0.0
         average_val_auc = 0.0
         average_test_auc = 0.0
-        average_train_loss = 0.0
-        average_val_loss = 0.0
-        average_test_loss = 0.0
+        #average_train_loss = 0.0
+        #average_val_loss = 0.0
+        #average_test_loss = 0.0
         average_epochs = 0.0
 
-        for train, val, test in zip(auc_dict['train_auc'], auc_dict['val_auc'], auc_dict['test_auc']):
+        for train, val, test, epoch in zip(auc_dict['train_auc'], auc_dict['val_auc'], auc_dict['test_auc'], auc_dict['epochs']):
             average_train_auc += train
             average_val_auc += val
             average_test_auc += test
+            average_epochs += epoch
 
+        """
         for train, val, test in zip(auc_dict['train_loss'], auc_dict['val_loss'], auc_dict['test_loss']):
             average_train_loss += train
             average_val_loss += val
             average_test_loss += test
-
-        for epoch in auc_dict['epochs']:
-            average_epochs += epoch
+        """
 
         multi_tissues_data['average_train_AUC'].append(average_train_auc / len(auc_dict['train_auc']))
         multi_tissues_data['average_val_AUC'].append(average_val_auc / len(auc_dict['val_auc']))
         multi_tissues_data['average_test_AUC'].append(average_test_auc / len(auc_dict['test_auc']))
-        multi_tissues_data['average_train_loss'].append(average_train_loss / len(auc_dict['train_loss']))
-        multi_tissues_data['average_val_loss'].append(average_val_loss / len(auc_dict['val_loss']))
-        multi_tissues_data['average_test_loss'].append(average_test_loss / len(auc_dict['test_loss']))
+        #multi_tissues_data['average_train_loss'].append(average_train_loss / len(auc_dict['train_loss']))
+        #multi_tissues_data['average_val_loss'].append(average_val_loss / len(auc_dict['val_loss']))
+        #multi_tissues_data['average_test_loss'].append(average_test_loss / len(auc_dict['test_loss']))
         multi_tissues_data['average_epochs'].append(average_epochs / len(auc_dict['epochs']))
 
     multi_tissues_df = pd.DataFrame(multi_tissues_data)
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
         print(multi_tissues_df)
+
+    print('Baseline Embeddings experiments:')
+    for exp_name in os.listdir(os.path.join('results', 'baseline_embedding_experiments')):
+        exp = exp_name.split('_')
+        embedding_data['feature'].append(exp[0])
+        embedding_data['LR'].append(exp[1])
+        embedding_data['EL'].append(exp[2])
+        embedding_data['embedding_size'].append(exp[3])
+        embedding_data['early_stop'].append(exp[4])
+        embedding_data['type_data'].append(exp[5][0:-5])
+
+        with open(os.path.join('results', 'baseline_embedding_experiments', exp_name,
+                               'output.json'), 'r') as dict_file:
+            auc_dict = json.load(dict_file)
+
+        average_train_auc = 0.0
+        average_val_auc = 0.0
+        average_test_auc = 0.0
+        #average_train_loss = 0.0
+        #average_val_loss = 0.0
+        #average_test_loss = 0.0
+        average_epochs = 0.0
+
+        for train, val, test, epoch in zip(auc_dict['train_auc'], auc_dict['val_auc'], auc_dict['test_auc'], auc_dict['epochs']):
+            average_train_auc += train
+            average_val_auc += val
+            average_test_auc += test
+            average_epochs += epoch
+        """
+        for train, val, test in zip(auc_dict['train_loss'], auc_dict['val_loss'], auc_dict['test_loss']):
+            average_train_loss += train
+            average_val_loss += val
+            average_test_loss += test
+        """
+
+        embedding_data['average_train_AUC'].append(average_train_auc / len(auc_dict['train_auc']))
+        embedding_data['average_val_AUC'].append(average_val_auc / len(auc_dict['val_auc']))
+        embedding_data['average_test_AUC'].append(average_test_auc / len(auc_dict['test_auc']))
+        #embedding_data['average_train_loss'].append(average_train_loss / len(auc_dict['train_loss']))
+        #embedding_data['average_val_loss'].append(average_val_loss / len(auc_dict['val_loss']))
+        #embedding_data['average_test_loss'].append(average_test_loss / len(auc_dict['test_loss']))
+        embedding_data['average_epochs'].append(average_epochs / len(auc_dict['epochs']))
+
+    embedding_df = pd.DataFrame(embedding_data)
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+        print(embedding_df)
 
     print('Dendronet Embeddings experiments:')
     for exp_name in os.listdir(os.path.join('results', 'dendronet_embedding_experiments')):
@@ -118,6 +167,10 @@ if __name__ == '__main__':
         dendro_data['embedding_size'].append(exp[4])
         dendro_data['early_stop'].append(exp[5])
         dendro_data['type_data'].append(exp[6][0:-5])
+        if len(exp) == 9:
+            dendro_data['tree'].append(exp[7] + '_' + exp[8])
+        else:
+            dendro_data['tree'].append(exp[7])
 
         with open(os.path.join('results', 'dendronet_embedding_experiments', exp_name,
                                'output.json'), 'r') as dict_file:
@@ -131,25 +184,25 @@ if __name__ == '__main__':
         average_test_loss = 0.0
         average_epochs = 0.0
 
-        for train, val, test in zip(auc_dict['train_auc'], auc_dict['val_auc'], auc_dict['test_auc']):
+        for train, val, test, epoch in zip(auc_dict['train_auc'], auc_dict['val_auc'], auc_dict['test_auc'], auc_dict['epochs']):
             average_train_auc += train
             average_val_auc += val
             average_test_auc += test
+            average_epochs += epoch
 
+        """
         for train, val, test in zip(auc_dict['train_loss'], auc_dict['val_loss'], auc_dict['test_loss']):
             average_train_loss += train
             average_val_loss += val
             average_test_loss += test
-
-        for epoch in auc_dict['epochs']:
-            average_epochs += epoch
+        """
 
         dendro_data['average_train_AUC'].append(average_train_auc / len(auc_dict['train_auc']))
         dendro_data['average_val_AUC'].append(average_val_auc / len(auc_dict['val_auc']))
         dendro_data['average_test_AUC'].append(average_test_auc / len(auc_dict['test_auc']))
-        dendro_data['average_train_loss'].append(average_train_loss / len(auc_dict['train_loss']))
-        dendro_data['average_val_loss'].append(average_val_loss / len(auc_dict['val_loss']))
-        dendro_data['average_test_loss'].append(average_test_loss / len(auc_dict['test_loss']))
+        #dendro_data['average_train_loss'].append(average_train_loss / len(auc_dict['train_loss']))
+        #dendro_data['average_val_loss'].append(average_val_loss / len(auc_dict['val_loss']))
+        #dendro_data['average_test_loss'].append(average_test_loss / len(auc_dict['test_loss']))
         dendro_data['average_epochs'].append(average_epochs / len(auc_dict['epochs']))
 
     dendro_df = pd.DataFrame(dendro_data)
